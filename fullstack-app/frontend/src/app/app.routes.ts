33580f7ec198c13fe03995dcc1792c70235459dc
Ajaz Beig <ajazbeig@Ajazs-MacBook-Air.local>
2025-04-06 08:34:02 +0530

    Routes updated

--- fullstack-app/frontend/src/app/app.routes.ts
+++ fullstack-app/frontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { RegisterComponent } from '../register/register.component';
 import { AuthGuard } from '../core/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent ,     canActivate: [AuthGuard]
